bff6d3020ae18cb03f1881c47b4c892ca2c089fe
reduno96 <reduno96@gmail.com>
2023-02-23 20:03:48 +0100

    Modification

--- javascript/main.js
+++ javascript/main.js
@@ -13,8 +13,6 @@ const hourEl = document.querySelector(".boxOne .clock .arrows .hour");
 const minuteEl = document.querySelector(".boxOne .clock .arrows .minute");
 const secondEl = document.querySelector(".boxOne .clock .arrows .second");
 
-console.log(hourEl);
-
 function updateClock() {
   const currentDate = new Date();
   const hour = currentDate.getHours();
@@ -35,3 +33,93 @@ document.querySelector(".dark-mode-switch").onclick = () => {
   document.querySelector(".boxTwo").classList.toggle("dark");
   document.querySelector(".boxTwo").classList.toggle("light");
 };
+
+isLeapYear = (year) => {
+  return (
+    (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
+    (year % 100 === 0 && year % 400 === 0)
+  );
+};
+
+getFebDay = (year) => {
+  return isLeapYear(year) ? 29 : 28;
+};
+
+let calendar = document.querySelector(".calendar");
+const month_names = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let month_picker = document.querySelector("#month-picker");
+
+// GENERATE CALENDAR
+
+generateCalendar = (month, year) => {
+  let calendar_days = document.querySelector(".calendar-days");
+  let calendar_header_year = document.querySelector("#year");
+  calendar_days.innerHTML = "";
+
+  let days_of_month = [
+    31,
+    getFebDay(year),
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31,
+  ];
+
+  let currDate = new Date();
+  if (!month === null || month === undefined) month = currDate.getMonth();
+  if (!year) year = currDate.getFullYear();
+
+  month_picker.innerHTML = month_names[month];
+  calendar_header_year.innerHTML = year;
+
+  let first_day = new Date(year, month, 1);
+
+  for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+    let day = document.createElement("div");
+    let kk = days_of_month[month] + first_day.getDay() - 1;
+    console.log(kk);
+    if (i >= first_day.getDay()) {
+      day.classList.add("calendar-day-hover");
+      day.innerHTML = i - first_day.getDay() + 1;
+      day.innerHTML += `<span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>`;
+      if (
+        i - first_day.getDay() + 1 === currDate.getDate() &&
+        year === currDate.getFullYear() &&
+        month === currDate.getMonth()
+      ) {
+        day.classList.add("curr-date");
+      }
+    }
+    calendar_days.appendChild(day);
+  }
+};
+
+let currDate = new Date();
+
+let curr_month = { value: currDate.getMonth() };
+let curr_year = { value: currDate.getFullYear() };
+
+generateCalendar(curr_month.value, curr_year.value);
