04fe00d034e0e44243e1cb715b1063413d7fc87c
reduno96 <reduno96@gmail.com>
2023-02-25 18:40:01 +0100

    Modification

--- javascript/main.js
+++ javascript/main.js
@@ -163,14 +163,15 @@ let getWeather = () => {
   let cityValue = cityRef.value;
   console.log(cityValue);
   if (cityValue.length === 0) {
-    result.innerHTML = `<h3> Please enter a city name </ h3>`;
-  }
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
-  fetch(url)
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data);
-      result.innerHTML = `
+    result.innerHTML = `<h3 class="msg"> Please enter a city name </ h3>`;
+  } else {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+    cityRef.value = "";
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        result.innerHTML = `
       <h2>${data.name}</h2> 
       <h4 class="weather">${data.weather[0].main}
       <h4/>
@@ -181,17 +182,18 @@ let getWeather = () => {
       <div class="temp-container">
         <div> 
         <h4 class="title">Min</h4>
-        <h4class="temp">${data.main.temp_min}</h4>
+        <h4 class="temp">${data.main.temp_min}</h4>
         </div>
         <div> 
         <h4 class="title">Max</h4>
-        <h4class="temp">${data.main.temp_max}</h4>
+        <h4 class="temp">${data.main.temp_max}</h4>
         </div>
       `;
-    })
-    .catch(() => {
-      result.innerHTML = `<h3>City Not Found</h3>`;
-    });
+      })
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">City Not Found</h3>`;
+      });
+  }
 };
 
 searchBtn.addEventListener("click", getWeather);
