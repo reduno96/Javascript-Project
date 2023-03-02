9920e1524a7995f91660fe822d53504ed025c694
reduno96 <reduno96@gmail.com>
2023-03-02 18:06:15 +0100

    clean input

--- javascript/main.js
+++ javascript/main.js
@@ -291,7 +291,7 @@ for (let key of keys) {
       display_output.innerHTML = "";
     } else if (value == "backspace") {
       input = input.slice(0, -1);
-      display_input.innerHTML = input;
+      display_input.innerHTML = CleanInput(input);
     } else if (value == "=") {
       let result = eval(input);
       display_output.innerHTML = result;
@@ -310,10 +310,34 @@ for (let key of keys) {
       ) {
         input += ")";
       }
-      display_input.innerHTML = input;
+      display_input.innerHTML = CleanInput(input);
     } else {
       input += value;
-      display_input.innerHTML = input;
+      display_input.innerHTML = CleanInput(input);
     }
   });
+}
+
+function CleanInput(input) {
+  let input_array = input.split("");
+  let input_array_length = input_array.length;
+
+  for (let i = 0; i < input_array_length; i++) {
+    if (input_array[i] == "*") {
+      input_array[i] = ` <span class ="operator">x</span> `;
+    } else if (input_array[i] == "/") {
+      input_array[i] = ` <span class ="operator">/</span> `;
+    } else if (input_array[i] == "+") {
+      input_array[i] = ` <span class ="operator">+</span> `;
+    } else if (input_array[i] == "-") {
+      input_array[i] = ` <span class ="operator">-</span> `;
+    } else if (input_array[i] == "(") {
+      input_array[i] = ` <span class ="operator">(</span> `;
+    } else if (input_array[i] == ")") {
+      input_array[i] = ` <span class ="operator">)</span> `;
+    } else if (input_array[i] == "%") {
+      input_array[i] = ` <span class ="operator">%</span> `;
+    }
+  }
+  return input_array.join("");
 }
