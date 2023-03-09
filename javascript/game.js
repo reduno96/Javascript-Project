00d6d39e9e6bd244123729538240c424f9a0cb14
reduno96 <reduno96@gmail.com>
2023-03-09 18:32:51 +0100

    Modification

--- javascript/game.js
+++ javascript/game.js
@@ -1,17 +1,23 @@
 const canvas = document.getElementById("canvas");
-const canasContext = canvas.getContext("2d");
+const canvasContext = canvas.getContext("2d");
 const pacmanFrames = document.getElementById("animation");
 const ghostFrames = document.getElementById("ghosts");
 
 let createRect = (x, y, width, height, color) => {
-  canasContext.fillStyle = color;
-  canasContext.fillRect(x, y, width, height);
+  canvasContext.fillStyle = color;
+  canvasContext.fillRect(x, y, width, height);
 };
 
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_BOTTOM = 1;
+
 let fps = 30;
+let pacman;
 let oneBlockSize = 20;
 let wallColor = "#342DCA";
-let wallSpaceWidth = oneBlockSize / 1.3;
+let wallSpaceWidth = oneBlockSize / 1.5;
 let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 let wallInnerColor = "black";
 
@@ -48,11 +54,14 @@ let gameLoop = () => {
 
 let update = () => {
   // todo
+  pacman.moveProcess();
 };
 
 let draw = () => {
   createRect(0, 0, canvas.width, canvas.height, "black");
   drawWalls();
+
+  pacman.draw();
 };
 
 let gameInterval = setInterval(gameLoop, 1000 / fps);
@@ -108,3 +117,35 @@ let drawWalls = () => {
     }
   }
 };
+
+let creatNewPacman = () => {
+  pacman = new Pacman(
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize / 5
+  );
+};
+
+creatNewPacman();
+gameLoop();
+
+window.addEventListener("keydown", (event) => {
+  let k = event.keyCode;
+  setTimeout(() => {
+    if (k == 37 || k == 81) {
+      // left
+      pacman.nextDirection = DIRECTION_LEFT;
+    } else if (k == 38 || k == 90) {
+      //up
+      pacman.nextDirection = DIRECTION_UP;
+    } else if (k == 39 || k == 68) {
+      //Right
+      pacman.nextDirection = DIRECTION_RIGHT;
+    } else if (k == 40 || k == 88) {
+      // bottom
+      pacman.nextDirection = DIRECTION_BOTTOM;
+    }
+  });
+});
---
+++ javascript/pacman.js
@@ -0,0 +1,136 @@
+class Pacman {
+  constructor(x, y, width, height, speed) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.direction = DIRECTION_RIGHT;
+    this.nextDirection = this.direction;
+    this.currentFrame = 1;
+    this.frameCount = 7;
+
+    setInterval(() => {
+      this.changeAnimation();
+    }, 100);
+  }
+
+  moveProcess() {
+    this.changeDirectionIfPossible();
+    this.moveForwards();
+    if (this.checkCollisions()) {
+      this.moveBackwards();
+    }
+  }
+
+  eat() {}
+
+  moveBackwards() {
+    switch (this.direction) {
+      case DIRECTION_RIGHT:
+        this.x -= this.speed;
+        break;
+      case DIRECTION_UP:
+        this.y += this.speed;
+        break;
+      case DIRECTION_LEFT:
+        this.x += this.speed;
+        break;
+      case DIRECTION_BOTTOM:
+        this.y -= this.speed;
+        break;
+    }
+  }
+
+  moveForwards() {
+    switch (this.direction) {
+      case DIRECTION_RIGHT:
+        this.x += this.speed;
+        break;
+      case DIRECTION_UP:
+        this.y -= this.speed;
+        break;
+      case DIRECTION_LEFT:
+        this.x -= this.speed;
+        break;
+      case DIRECTION_BOTTOM:
+        this.y += this.speed;
+        break;
+    }
+  }
+
+  checkCollisions() {
+    let isCollided = false;
+    if (
+      map[parseInt(this.y / oneBlockSize)][parseInt(this.x / oneBlockSize)] ==
+        1 ||
+      map[parseInt(this.y / oneBlockSize + 0.9999)][
+        parseInt(this.x / oneBlockSize)
+      ] == 1 ||
+      map[parseInt(this.y / oneBlockSize)][
+        parseInt(this.x / oneBlockSize + 0.9999)
+      ] == 1 ||
+      map[parseInt(this.y / oneBlockSize + 0.9999)][
+        parseInt(this.x / oneBlockSize + 0.9999)
+      ] == 1
+    ) {
+      isCollided = true;
+    }
+    return isCollided;
+  }
+
+  changeDirectionIfPossible() {
+    if (this.direction == this.nextDirection) return;
+
+    let tempDirection = this.direction;
+    this.direction = this.nextDirection;
+    this.moveForwards();
+    if (this.checkCollisions()) {
+      this.moveBackwards();
+    }
+  }
+
+  changeAnimation() {
+    this.currentFrame =
+      this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1;
+  }
+
+  draw() {
+    canvasContext.save();
+    canvasContext.translate(
+      this.x + oneBlockSize / 2,
+      this.y + oneBlockSize / 2
+    );
+    canvasContext.rotate((this.direction * 90 * Math.PI) / 180);
+
+    canvasContext.translate(
+      -this.x - oneBlockSize / 2,
+      -this.y - oneBlockSize / 2
+    );
+
+    canvasContext.drawImage(
+      pacmanFrames,
+      (this.currentFrame - 1) * oneBlockSize,
+      0,
+      oneBlockSize,
+      oneBlockSize,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+
+  getMapX() {
+    return parseInt(this.x / oneBlockSize);
+  }
+  getMapY() {
+    return parseInt(this.y / oneBlockSize);
+  }
+  getMapXRightSide() {
+    return parseInt((this.x + 0.9999 * oneBlockSize) / oneBlockSize);
+  }
+  getMapYRightSide() {
+    return parseInt((this.Y + 0.9999 * oneBlockSize) / oneBlockSize);
+  }
+}
