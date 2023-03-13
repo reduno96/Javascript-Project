9fc89998c3a95649cb8e8100d3abed609b044f3a
reduno96 <reduno96@gmail.com>
2023-03-13 20:03:20 +0100

    Add Password Generator

--- javascript/main.js
+++ javascript/main.js
@@ -393,3 +393,53 @@ function PerpareInput(input) {
   }
   return input_array.join("");
 }
+
+// Paasword
+
+const password_el = document.querySelector(
+  " .projects .boxSeven label #password"
+);
+const length_el = document.querySelector(".projects .boxSeven #length");
+const uppercase_el = document.querySelector(
+  ".projects .boxSeven label #uppercase"
+);
+const lowercase_el = document.querySelector(
+  ".projects .boxSeven label #lowercase"
+);
+const numbers_el = document.querySelector(".projects .boxSeven label #numbers");
+const symbols_el = document.querySelector(".projects .boxSeven label #symbols");
+
+const generate_btn = document.querySelector(".projects .boxSeven #generate");
+generate_btn.addEventListener("click", GeneratePassword);
+const copy_btn = document.querySelector(".projects .boxSeven #copy");
+copy_btn.addEventListener("click", CopyPassword);
+
+const uppercase_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase_chars = "abcdefghijklmnopqrstuvwxyz";
+const numbers_chars = "0123456789";
+const symbols_chars = "§@#$%^&*()";
+
+function GeneratePassword() {
+  let password = "";
+  let length = length_el.value;
+  let chars = "";
+
+  chars += uppercase_el.checked ? uppercase_chars : "";
+  chars += lowercase_el.checked ? lowercase_chars : "";
+  chars += numbers_el.checked ? numbers_chars : "";
+  chars += symbols_el.checked ? symbols_chars : "";
+
+  for (let i = 0; i < length; i++) {
+    let rand = Math.floor(Math.random() * chars.length);
+    password += chars.substring(rand, rand + 1);
+    console.log(chars.length);
+  }
+  password_el.value = password;
+}
+
+async function CopyPassword() {
+  if (navigator.clipboard) {
+    await navigator.clipboard.writeText(password_el.value);
+    alert("password copied to clipboard");
+  }
+}
